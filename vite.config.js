1e3106eb950f159cd2f5fa4f03b4cc18d5878865
xharry1 <hemants1n3h@gmail.com>
2025-03-05 16:11:09 -0800

    Ensure Vite loads environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/',  // Ensures proper routing
-  
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 3000, 
-  }
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on mode (production or development)
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    plugins: [react(), tailwindcss()],
+    base: '/',
+    server: {
+      host: '0.0.0.0',
+      port: process.env.PORT || 3000, 
+    }
+  };
 });
